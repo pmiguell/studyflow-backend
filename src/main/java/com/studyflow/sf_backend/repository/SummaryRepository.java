61256c78e5c32c6f99d0a125571ef799d7632499
Pedro Miguel <pedromiguellitoo@gmail.com>
2026-02-23 21:03:42 -0300

    refact: enhance layered architecture for summary scope

--- src/main/java/com/studyflow/sf_backend/repository/SummaryRepository.java
+++ src/main/java/com/studyflow/sf_backend/repository/SummaryRepository.java
@@ -1,9 +1,14 @@
 package com.studyflow.sf_backend.repository;
 
 import com.studyflow.sf_backend.entity.Summary;
+import com.studyflow.sf_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SummaryRepository extends JpaRepository<Summary, Long> {
+    List<Summary> findBySubject_User(User user);
+
 }
